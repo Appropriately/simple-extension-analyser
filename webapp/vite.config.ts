437ec730c0a7e27aa459ce4958ac06e03b7967f7
Sean Lewis <sean.alan.lewis@gmail.com>
2025-04-19 18:46:54 +0100

    chore(gh-pages): remove gh-pages package and config
    moving to a github actions approach that uses the main branch

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://appropriately.github.io/simple-extension-analyser",
   plugins: [react()],
   resolve: {
     alias: {
