b6454877d1b0efc744eea6866204bd740a6a870e
Sean Lewis <sean.alan.lewis@gmail.com>
2025-04-22 09:37:13 +0100

    chore: move to more common react app.tsx usage

--- webapp/src/app/app.tsx
+++ webapp/src/app/app.tsx
@@ -1,47 +1,16 @@
-import { lazy, Suspense, useEffect } from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import "@/styles/main.scss";
 
-import Layout from "./routes/layout";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const Home = lazy(() => import("./routes/home"));
-const Extension = lazy(() => import("./routes/extension"));
+import { ToastsProvider } from "@/features/toasts";
 
-function App() {
-  useEffect(() => {
-    (function () {
-      const htmlElement = document.querySelector("html");
-      if (htmlElement && htmlElement.getAttribute("data-bs-theme") === "auto") {
-        function updateTheme() {
-          if (htmlElement) {
-            htmlElement.setAttribute(
-              "data-bs-theme",
-              window.matchMedia("(prefers-color-scheme: dark)").matches
-                ? "dark"
-                : "light"
-            );
-          }
-        }
+import Router from "./router";
 
-        window
-          .matchMedia("(prefers-color-scheme: dark)")
-          .addEventListener("change", updateTheme);
-        updateTheme();
-      }
-    })();
-  });
-
-  return (
-    <HashRouter>
-      <Suspense fallback={<div className="loading">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/extension/:id" element={<Extension />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </HashRouter>
-  );
-}
-
-export default App;
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ToastsProvider limit={5}>
+      <Router />
+    </ToastsProvider>
+  </StrictMode>
+);
---
+++ webapp/src/app/router.tsx
@@ -0,0 +1,47 @@
+import { lazy, Suspense, useEffect } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+import Layout from "./routes/layout";
+
+const Home = lazy(() => import("./routes/home"));
+const Extension = lazy(() => import("./routes/extension"));
+
+function Router() {
+  useEffect(() => {
+    (function () {
+      const htmlElement = document.querySelector("html");
+      if (htmlElement && htmlElement.getAttribute("data-bs-theme") === "auto") {
+        function updateTheme() {
+          if (htmlElement) {
+            htmlElement.setAttribute(
+              "data-bs-theme",
+              window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? "dark"
+                : "light"
+            );
+          }
+        }
+
+        window
+          .matchMedia("(prefers-color-scheme: dark)")
+          .addEventListener("change", updateTheme);
+        updateTheme();
+      }
+    })();
+  });
+
+  return (
+    <HashRouter>
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/extension/:id" element={<Extension />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  );
+}
+
+export default Router;
