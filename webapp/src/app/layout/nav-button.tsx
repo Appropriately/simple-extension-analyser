b2ec44fd26307f2a73f5da455331cef310a5b271
Sean Lewis <sean.alan.lewis@gmail.com>
2025-05-02 21:26:18 +0100

    chore(navbar): tweak active/hover text colours for button

--- webapp/src/app/layout/nav-button.tsx
+++ webapp/src/app/layout/nav-button.tsx
@@ -9,7 +9,7 @@ function NavButton({ children, className, ...props }: Props) {
   return (
     <button
       type="button"
-      className={`flex items-center gap-2 text-zinc-300 hover:bg-zinc-800 active:bg-zinc-700 vertical-align cursor-pointer text-left h-full ${className}`}
+      className={`flex items-center gap-2 text-zinc-300 hover:text-zinc-200 active:text-zinc-100 hover:bg-zinc-800 active:bg-zinc-700 vertical-align cursor-pointer text-left h-full ${className}`}
       {...props}
     >
       {children}
