79c6d9569b2ef3650494fb8cc99d3090d680177d
Sean Lewis <sean.alan.lewis@gmail.com>
2025-05-18 10:54:03 +0100

    fix(i18n): use navigator.language for simple locale detection on load

--- webapp/src/lib/i18n.ts
+++ webapp/src/lib/i18n.ts
@@ -2,12 +2,17 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import enGB from "../locales/en-GB.json";
+import roRO from "../locales/ro-RO.json";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "en-GB",
+  lng: navigator.language,
   resources: {
     "en-GB": {
       translation: enGB,
+    },
+    "ro-RO": {
+      translation: roRO,
     },
   },
 });
