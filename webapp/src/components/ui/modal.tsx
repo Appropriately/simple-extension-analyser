00be49afaf526ae77d224b203c8ffeae0df74dd1
Sean Lewis <sean.alan.lewis@gmail.com>
2025-05-26 23:34:31 +0100

    chore(modal): move closing capabilities over to a context model

--- webapp/src/components/ui/modal.tsx
+++ webapp/src/components/ui/modal.tsx
@@ -1,20 +1,27 @@
 import {
-  cloneElement,
-  isValidElement,
+  ButtonHTMLAttributes,
+  createContext,
   ReactNode,
+  useContext,
   useEffect,
   useRef,
-  Children,
 } from "react";
-import { Card } from "@/components/ui";
 
-interface BaseProps {
+import { Button, Card } from "@/components/ui";
+
+interface Props {
   children: ReactNode;
 }
 
-interface ModalProps extends BaseProps {
-  open?: boolean;
+interface ModalProps extends Props {
+  open?: boolean; // whether the model is initially open or not.
 }
+
+interface ModalContextType {
+  close: () => void;
+}
+
+const ModalContext = createContext<ModalContextType | null>(null);
 
 function Modal({ children, open = false }: ModalProps) {
   const ref = useRef<HTMLDialogElement>(null);
@@ -24,67 +31,85 @@ function Modal({ children, open = false }: ModalProps) {
     else ref.current?.close();
   }, [open]);
 
-  const handleClose = () => ref.current?.close();
+  const close = () => ref.current?.close();
 
-  const childrenWithProps = Children.map(children, (child) => {
-    if (
-      isValidElement<ModalOnCloseProps>(child) &&
-      (child.type === ModalHeader ||
-        child.type === ModalBody ||
-        child.type === ModalFooter)
-    )
-      return cloneElement(child, { onClose: handleClose });
+  const handleClickOutside = (e: React.MouseEvent<HTMLDialogElement>) => {
+    const dialogDimensions = ref.current?.getBoundingClientRect();
+    if (dialogDimensions) {
+      const clickedOutside =
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom;
 
-    return child;
-  });
+      if (clickedOutside) close();
+    }
+  };
 
   return (
-    <>
-      <dialog
-        ref={ref}
-        className="bg-transparent text-zinc-900 dark:text-zinc-200 min-w-96 max-w-[8000px] min-h-32 top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 backdrop:bg-zinc-900/60"
-      >
-        <Card>{childrenWithProps}</Card>
-      </dialog>
-
-      <button onClick={() => ref.current?.showModal()}>Open</button>
-    </>
+    <dialog
+      ref={ref}
+      onClick={handleClickOutside}
+      className="bg-transparent text-zinc-900 dark:text-zinc-200 min-w-96 max-w-[8000px] min-h-32 top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 backdrop:bg-zinc-900/60"
+    >
+      <ModalContext.Provider value={{ close }}>
+        <Card>{children}</Card>
+      </ModalContext.Provider>
+    </dialog>
   );
 }
 
-interface ModalOnCloseProps {
-  onClose?: () => void;
+function ModalClose() {
+  const context = useContext(ModalContext);
+  if (!context) throw new Error("ModalClose must be used within a Modal");
+
+  return (
+    <button
+      onClick={context.close}
+      className="px-1 hover:bg-zinc-800 rounded cursor-pointer"
+      aria-label="Close"
+    >
+      ✕
+    </button>
+  );
 }
 
-interface ModalHeaderProps extends BaseProps, ModalOnCloseProps {}
+function ModalCloseButton({
+  children,
+  ...props
+}: Props & ButtonHTMLAttributes<HTMLButtonElement>) {
+  const context = useContext(ModalContext);
+  if (!context) throw new Error("ModalCloseButton must be used within a Modal");
 
-function ModalHeader({ children, onClose }: ModalHeaderProps) {
+  return (
+    <Button onClick={context.close} aria-label="Close" {...props}>
+      {children}
+    </Button>
+  );
+}
+
+function ModalHeader({ children }: Props) {
+  const context = useContext(ModalContext);
+  if (!context) throw new Error("ModalHeader must be used within a Modal");
+
   return (
     <Card.Header className="flex justify-between items-center">
       <div>{children}</div>
-      <button
-        onClick={onClose}
-        className="px-1 hover:bg-zinc-800 rounded cursor-pointer"
-        aria-label="Close"
-      >
-        ✕
-      </button>
+      <ModalClose />
     </Card.Header>
   );
 }
 
-interface ModalBodyProps extends BaseProps, ModalOnCloseProps {}
-
-function ModalBody({ children }: ModalBodyProps) {
+function ModalBody({ children }: Props) {
   return <Card.Body>{children}</Card.Body>;
 }
 
-interface ModalFooterProps extends BaseProps, ModalOnCloseProps {}
-
-function ModalFooter({ children }: ModalFooterProps) {
+function ModalFooter({ children }: Props) {
   return <Card.Footer>{children}</Card.Footer>;
 }
 
+Modal.Close = ModalClose;
+Modal.CloseButton = ModalCloseButton;
 Modal.Header = ModalHeader;
 Modal.Body = ModalBody;
 Modal.Footer = ModalFooter;
